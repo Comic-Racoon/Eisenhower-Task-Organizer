98fc5e048773fc106575bb5755301a75167fb3c7
Rahul Umesh Mhatre <rahul.mhatre@phenompeople.com>
2023-10-09 17:06:43 +0530

    updated folder structure and various files

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,50 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const htmlPlugin = new HtmlWebPackPlugin({
- template: "./src/index.html",
- filename: "./index.html"
-});
-module.exports = {
-mode: "development",
-  module: {
-    rules: [{
-   test: /\.js$/,
-   exclude: /node_modules/,
-   use: {
-     loader: "babel-loader"
-   }
- },
-  {
-   test: /\.css$/,
-   use: ["style-loader", "css-loader"]
-  }
-]},
- plugins: [htmlPlugin]
-};
+const path = require("path")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+module.exports = [
+  {
+    entry: "./src/client/index.tsx",
+    mode: "development",
+    target: "web",
+    output: {
+      path: path.resolve(__dirname, "dist/client"),
+      filename: "client_bundle.js"
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/client/index.html"
+      })
+    ],
+    resolve: {
+      extensions: [".js", ".ts", ".tsx", ".jsx"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          exclude: /node_modules/,
+          use: 'ts-loader',
+        },
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-react-env', '@babel/preset-react', 'babel/preset-typescript']
+            },
+          },
+        },
+        {
+          test: /\.css$/,
+          exclude: /node_modules/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|ttf)$/i,
+          type: "assets/resource"
+        },
+      ]
+    }
+  }
+]
